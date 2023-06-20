95602261fa00ead4f4e67f94d488af0cb737a4c5
Damshine <2314028487@qq.com>
2023-06-20 22:00:04 +0800

    六大设计原则【里氏替换原则】

---
+++ tutorials-2.3.0/src/main/java/com/github/mrs/CashCard.java
@@ -0,0 +1,60 @@
+package com.github.mrs;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * description: 模拟储蓄卡功能
+ * date: 2023/6/20 21:05
+ * author: MR.孙
+ */
+public class CashCard {
+
+    private static final Logger logger = LoggerFactory.getLogger(CashCard.class);
+
+    /**
+     * 提现
+     *
+     * @param orderId 单号
+     * @param amount  金额
+     * @return 状态码 0000成功、0001失败、0002重复
+     */
+    public String withdrawal(String orderId, BigDecimal amount) {
+        // 模拟支付成功
+        logger.info("提现成功，单号：{} 金额：{}", orderId, amount);
+        return "0000";
+    }
+
+    /**
+     * 储蓄
+     *
+     * @param orderId 单号
+     * @param amount  金额
+     */
+    public String recharge(String orderId, BigDecimal amount) {
+        // 模拟充值成功
+        logger.info("储蓄成功，单号：{} 金额：{}", orderId, amount);
+        return "0000";
+    }
+
+    /**
+     * 交易流水查询
+     * @return 交易流水
+     */
+    public List<String> tradeFlow() {
+        logger.info("交易流水查询成功");
+        List<String> tradeList = new ArrayList<String>();
+        tradeList.add("100001,100.00");
+        tradeList.add("100001,80.00");
+        tradeList.add("100001,76.50");
+        tradeList.add("100001,126.00");
+        return tradeList;
+    }
+
+
+}
